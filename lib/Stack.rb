1fecadd0fb2e59c7a1b072ab4e33e318f89e0c99
Emma Claire Philippides <acme9898@gmail.com>
2017-02-27 13:33:14 -0800

    complete

--- lib/Stack.rb
+++ lib/Stack.rb
@@ -1,21 +1,28 @@
+
 class Stack
   def initialize
     @store = Array.new
   end
 
   def push(element)
+    @store.push(element)
   end
 
-  def pop
+  def pop #return what was removed
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
+    #return @store.empty?
   end
 
   def to_s
